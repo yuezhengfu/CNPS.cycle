091a9f51bffe3d719d5049fe2e269936d286a794
Zhengfu Yue <39332214+yuezhengfu@users.noreply.github.com>
2023-11-04 17:33:34 +0800

    Add files via upload

---
+++ R/PCoA.R
@@ -0,0 +1,114 @@
+#' Principal coordinate analysis (PCoA) and multivariate data analysis
+#' @param ARG_sub C.abundance[,2:ncol(C.abundance)], N.abundance[,2:ncol(N.abundance)], P.abundance[,2:ncol(P.abundance)], S.abundance[,2:ncol(S.abundance)]
+#' @param group Sample grouping file
+##' data(ko)
+##' data(group)
+##' data(Gene)
+##' data(tax)
+##' data(abundance)
+#' result <- pcoa.arg(C.abundance[,2:ncol(C.abundance)],group)
+#' @author contact: Zhengfu Yue \email{yuezhengfu2011@163.com}
+#' @export
+
+pcoa.arg <- function(ARG_sub,group){
+    colnames(group) <- c("sample","Group")
+    ARG_beta <- t(ARG_sub)
+    ARG_dis <- vegdist(ARG_beta)
+
+    pcoa<- pcoa(ARG_dis, correction = "none", rn = NULL)
+    PC1 = pcoa$vectors[,1]
+    PC2 = pcoa$vectors[,2]
+    plotdata <- data.frame(rownames(pcoa$vectors),PC1,PC2)
+    colnames(plotdata) <-c("sample","PC1","PC2")
+    plotdata <- merge(plotdata,group)
+    pc1 <-floor(pcoa$values$Relative_eig[1]*100)
+    pc2 <-floor(pcoa$values$Relative_eig[2]*100)
+
+    ARG.adonis <- adonis(ARG_dis~Group,data = plotdata,distance = "bray")
+    ARG.anosim <- with(plotdata,anosim(ARG_dis,Group))
+    ARG.mrpp <- with(plotdata,mrpp(ARG_dis,Group))
+    diff.test <- data.frame(Test = c("Adonis","ANOSIM","MRPP"),
+                            R2 = c(round(ARG.adonis$aov.tab$R2[1],4),
+                                   round(ARG.anosim$statistic[1],4),
+                                   round(ARG.mrpp$E.delta[1],4)),
+                            p.value = c(ARG.adonis$aov.tab$`Pr(>F)`[1],
+                                        ARG.anosim$signif[1],
+                                        ARG.mrpp$Pvalue[1]))
+
+    ARG_pcoa1<-ggplot(plotdata, aes(PC1, PC2)) +
+        geom_point(aes(fill=Group),size=4.5,color = "black",shape = 21,alpha = 0.8)+
+        geom_vline(aes(xintercept = 0),linetype="dotted")+
+        geom_hline(aes(yintercept = 0),linetype="dotted")+
+        scale_fill_manual(values=cbbPalette)+
+        labs(title="PCoA - Function genes of cycling") +
+        xlab(paste("PC1 ( ",pc1,"%"," )",sep="")) +
+        ylab(paste("PC2 ( ",pc2,"%"," )",sep=""))+
+        theme(text=element_text(size=18))+
+        theme(panel.background = element_rect(fill='white', colour='black'),
+              panel.grid=element_blank(),
+              axis.title = element_text(color='black',size=18),
+              axis.ticks.length = unit(0.4,"lines"), axis.ticks = element_line(color='black'),
+              axis.line = element_line(colour = "black"),
+              axis.title.x=element_text(colour='black', size=18),
+              axis.title.y=element_text(colour='black', size=18),
+              axis.text=element_text(colour='black',size=16),
+              legend.title=element_text(size = 14,face = "bold"),
+              legend.text=element_text(size=12),
+              legend.key=element_blank(),legend.position = "right",
+              legend.background = element_rect(colour = "black"))+
+        theme(plot.title = element_text(size=20,colour = "black",hjust = 0.5,face = "bold"))
+
+    ARG_pcoa2<-ggplot(plotdata, aes(PC1, PC2)) +
+        geom_point(aes(fill=Group),size=4.5,color = "black",shape = 21,alpha = 0.8)+
+        stat_ellipse(aes(fill = Group),geom = "polygon",level = 0.95,alpha = 0.3)+
+        geom_vline(aes(xintercept = 0),linetype="dotted")+
+        geom_hline(aes(yintercept = 0),linetype="dotted")+
+        scale_fill_manual(values=cbbPalette)+
+        labs(title="PCoA - Function genes of cycling") +
+        xlab(paste("PC1 ( ",pc1,"%"," )",sep="")) +
+        ylab(paste("PC2 ( ",pc2,"%"," )",sep=""))+
+        theme(text=element_text(size=18))+
+        theme(panel.background = element_rect(fill='white', colour='black'),
+              panel.grid=element_blank(),
+              axis.title = element_text(color='black',size=18),
+              axis.ticks.length = unit(0.4,"lines"), axis.ticks = element_line(color='black'),
+              axis.line = element_line(colour = "black"),
+              axis.title.x=element_text(colour='black', size=18),
+              axis.title.y=element_text(colour='black', size=18),
+              axis.text=element_text(colour='black',size=16),
+              legend.title=element_text(size = 14,face = "bold"),
+              legend.text=element_text(size=12),
+              legend.key=element_blank(),legend.position = "right",
+              legend.background = element_rect(colour = "black"))+
+        theme(plot.title = element_text(size=20,colour = "black",hjust = 0.5,face = "bold"))
+
+    ARG_pcoa3<-ggplot(plotdata, aes(PC1, PC2)) +
+        geom_point(aes(fill=Group),size=4.5,color = "black",shape = 21,alpha = 0.8)+
+        geom_label_repel(aes(PC1,PC2,label = sample),fill = "white",color = "black",
+                         box.padding = unit(0.3,"lines"),segment.colour = "grey50",
+                         label.padding = unit(0.15,"lines"),size = 2) +
+        geom_vline(aes(xintercept = 0),linetype="dotted")+
+        geom_hline(aes(yintercept = 0),linetype="dotted")+
+        scale_fill_manual(values=cbbPalette)+
+        labs(title="PCoA - Function genes of cycling") +
+        xlab(paste("PC1 ( ",pc1,"%"," )",sep="")) +
+        ylab(paste("PC2 ( ",pc2,"%"," )",sep=""))+
+        theme(text=element_text(size=18))+
+        theme(panel.background = element_rect(fill='white', colour='black'),
+              panel.grid=element_blank(),
+              axis.title = element_text(color='black',size=18),
+              axis.ticks.length = unit(0.4,"lines"), axis.ticks = element_line(color='black'),
+              axis.line = element_line(colour = "black"),
+              axis.title.x=element_text(colour='black', size=18),
+              axis.title.y=element_text(colour='black', size=18),
+              axis.text=element_text(colour='black',size=16),
+              legend.title=element_text(size = 14,face = "bold"),
+              legend.text=element_text(size=12),
+              legend.key=element_blank(),legend.position = "right",
+              legend.background = element_rect(colour = "black"))+
+        theme(plot.title = element_text(size=20,colour = "black",hjust = 0.5,face = "bold"))
+
+
+    result <- list(ARG_dis,diff.test,plotdata,ARG_pcoa1,ARG_pcoa2,ARG_pcoa3)
+    return(result)
+}
---
+++ R/Pcycleabundance.R
@@ -0,0 +1,42 @@
+#' ko abundance table related to phosphorus element was extracted
+#' @param kegg.entry A profile table containing kegg's entry
+#' @examples
+##' data(ko)
+##' data(group)
+##' data(Gene)
+##' data(tax)
+##' data(abundance)
+#' P.abundance = Pcyc.abundance(kegg.entry = ko)
+#' @author contact: Zhengfu Yue \email{yuezhengfu2011@163.com}
+#' @export
+
+
+Pcyc.abundance <- function(kegg.entry = ko){
+  # List of KEGG entry identifiers
+    P <- c("K07636","K07657","K07658","K07768","K07776")
+  # Initialize an empty data frame for KEGG entries
+    P1 <- c()
+    for (i in 1:length(P)) {
+        if (P[i] %in% rownames(ko)) {
+            P2 <- ko[P[i],]
+        }else{
+            P2 <- rep(0,ncol(ko))
+        }
+        P1 <- rbind(P1,P2)
+    }
+    rownames(P1) <- P
+    P1 <- as.data.frame(t(P1))
+  # Calculate P.abundance based on P1
+    P.abundance <- data.frame(RB = P1$K07636 + (P1$K07657 + P1$K07658)/2,
+                              X3 = P1$K07768 + P1$K07776)
+  # Set row names for P.abundance
+    rownames(P.abundance) <- rownames(P1)
+  # Add a 'Type' column with meaningful names
+    P.abundance <- as.data.frame(t(P.abundance))
+    P.abundance$Type <- rownames(P.abundance)
+    P.abundance <- P.abundance[,c("Type",colnames(P.abundance)[1:(ncol(P.abundance)-1)])]
+    P.abundance$Type <- c("PhoR-PhoB system","SenX3-RegX3 system")
+  # Reorder the 'Type' column
+    P.abundance$Type <- factor(P.abundance$Type,levels = rev(P.abundance$Type))
+    return(P.abundance)
+}
